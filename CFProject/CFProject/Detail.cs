cf433c5c6ef490a3dbaa505f164876fe663ee46c
nguyenhuuhoa <nguyenhuuhoa1998@gmail.com>
2019-06-18 03:41:49 +0700

    finish display detail

--- CFProject/CFProject/Detail.cs
+++ CFProject/CFProject/Detail.cs
@@ -15,7 +15,6 @@ namespace CFProject
     public partial class Detail : Form
     {
         private int id = -1;
-        private PrintDocument printDocument = new PrintDocument();
        
 
         public Detail()
@@ -29,6 +28,62 @@ namespace CFProject
             this.id = i;
             btnPrint.Click += new EventHandler(printButton_Click);
             printDocument.PrintPage += new PrintPageEventHandler(printDocument_PrintPage);
+        }
+
+        private PrintDocument printDocument = new PrintDocument();
+
+        private void Detail_Load(object sender, EventArgs e)
+        {
+            Bitmap avt = new Bitmap(Application.StartupPath + "\\Resources\\avatar.jpg");
+            pbAvatar.Image = avt;
+
+            using (var db = new QLCafeEntities())
+            {
+                var billInfo = db.HoaDons.Find(id);
+                var accInfo = db.TaiKhoans.Find(billInfo.MaTaiKhoan);
+                lblMaNV.Text = "NV" + accInfo.MaTaiKhoan.ToString();
+                lblName.Text = accInfo.NguoiQuanLi.HoTen;
+                lblDate.Text = billInfo.NgayLapHoaDon.ToString();
+                lblCost.Text = billInfo.TongTien.ToString() + " VNĐ";
+
+                var l = db.ChiTietHoaDons.Where(d => d.MaHoaDon == id).ToList();
+                int[] xLocation = { 10, 100, 300, 400 };
+                var yLocation = 10;
+                int stt = 1;
+                foreach (var index in l)
+                {
+                    Label lblID = new Label();
+                    lblID.Size = new Size(90, 20);
+                    lblID.Text = stt.ToString();
+                    lblID.Font = new Font("Time New Roman", 9.75F, FontStyle.Regular);
+                    lblID.Location = new System.Drawing.Point(xLocation[0], yLocation);
+                    pnDetail.Controls.Add(lblID);
+
+                    Label lblName = new Label();
+                    lblName.Size = new Size(200, 20);
+                    lblName.Text = index.SanPham.TenSanPham;
+                    lblName.Font = new Font("Time New Roman", 9.75F, FontStyle.Regular);
+                    lblName.Location = new System.Drawing.Point(xLocation[1], yLocation);
+                    pnDetail.Controls.Add(lblName);
+
+                    Label lblNumber = new Label();
+                    lblNumber.Size = new Size(100, 20);
+                    lblNumber.Text = index.SoLuong.ToString();
+                    lblNumber.Font = new Font("Time New Roman", 9.75F, FontStyle.Regular);
+                    lblNumber.Location = new System.Drawing.Point(xLocation[2], yLocation);
+                    pnDetail.Controls.Add(lblNumber);
+
+                    Label lblCost = new Label();
+                    lblCost.Size = new Size(100, 20);
+                    lblCost.Text = index.DonGia.ToString() + " đ";
+                    lblCost.Font = new Font("Time New Roman", 9.75F, FontStyle.Regular);
+                    lblCost.Location = new System.Drawing.Point(xLocation[3], yLocation);
+                    pnDetail.Controls.Add(lblCost);
+
+                    yLocation += 40;
+                    stt++;
+                }
+            }
         }
 
         void printButton_Click(object sender, EventArgs e)
@@ -65,5 +120,7 @@ namespace CFProject
         {
             e.Graphics.DrawImage(memoryImage, 0, 0);
         }
+
+        
     }
 }
