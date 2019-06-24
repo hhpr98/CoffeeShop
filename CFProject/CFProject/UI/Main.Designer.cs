5981610a528a59be4dd4da58d0a65f85956bc806
nguyenhuuhoa <nguyenhuuhoa1998@gmail.com>
2019-06-24 09:11:32 +0700

    finish KetQuaThucHien

--- CFProject/CFProject/UI/Main.Designer.cs
+++ CFProject/CFProject/UI/Main.Designer.cs
@@ -397,6 +397,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
             this.ClientSize = new System.Drawing.Size(1136, 563);
             this.Controls.Add(this.btnSwitch);
             this.Controls.Add(this.lblTableSelected);
